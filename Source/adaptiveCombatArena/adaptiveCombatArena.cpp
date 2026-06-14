91ea8a1a77d244af787a2afcf255d826519b2751
Tolani Solanke <1569758@stu.tmc.ac.uk>
2026-06-14 14:27:50 +0100

    Init: Unreal project

---
+++ Source/adaptiveCombatArena/adaptiveCombatArena.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "adaptiveCombatArena.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, adaptiveCombatArena, "adaptiveCombatArena" );
